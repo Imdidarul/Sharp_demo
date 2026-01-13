a4be4210cde47813db725c77eb1dbee9963c8f17
Didarul Islam Laskar <95709968+Imdidarul@users.noreply.github.com>
2026-01-13 18:06:37 +0530

    Middleware

---
+++ middleware.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+let port = 3000
+
+app.use("/welcome",(req,res,next)=>{
+    req.user = "Guest"
+    next()
+})
+
+app.get("/welcome",(req,res)=>{
+    res.send(`<h1>Welcome,${req.user}!</h1>`)
+})
+
+app.listen(port,()=>{
+    console.log("Server is running on port 3000")
+})

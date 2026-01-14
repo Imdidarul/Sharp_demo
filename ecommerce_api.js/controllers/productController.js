987f6bdaebc496eee1968bc364ba2f77d6237db2
Didarul Islam Laskar <95709968+Imdidarul@users.noreply.github.com>
2026-01-14 21:30:12 +0530

    Added Controllers for all

--- ecommerce_api.js/controllers/productController.js
+++ ecommerce_api.js/controllers/productController.js
@@ -1,18 +1,18 @@
-const getProducts = (req,res)=>{
+const getAllProducts = (req,res)=>{
     res.send( "Fetching all products")
 }
 
-const postProducts = (req,res)=>{
+const addProduct = (req,res)=>{
     res.send( "Adding a new product")
 }
 
-const getById = (req,res)=>{
+const getProductById = (req,res)=>{
     const id = req.params.id
     res.send(`Fetching product with ID: ${id}`)
 }
 
 module.exports = {
-    getProducts,
-    postProducts,
-    getById
+    getAllProducts,
+    addProduct,
+    getProductById
 }
--- ecommerce_api.js/routes/productRoutes.js
+++ ecommerce_api.js/routes/productRoutes.js
@@ -2,10 +2,10 @@ const express = require("express")
 const router = express.Router()
 const productController = require("../controllers/productController")
 
-router.get("/",productController.getProducts)
+router.get("/",productController.getAllProducts)
 
-router.post("/",productController.postProducts)
+router.post("/",productController.addProduct)
 
-router.get("/:id",productController.getById)
+router.get("/:id",productController.getProductById)
 
 module.exports = router
